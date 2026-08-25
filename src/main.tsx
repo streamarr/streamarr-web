8894178848321d2c396f0e3c001f1c23f0f6bea7
Andrew Stucky <andrew.david.stucky@gmail.com>
2026-08-25 13:30:16 -0400

    structural: delete the global auth.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import '@mantine/core/styles.css'
 import './styles/tokens.generated.css'
 import './styles/fonts.css'
-import './ui/auth.css'
 
 import { ApolloProvider } from '@apollo/client/react'
 import { MantineProvider } from '@mantine/core'
